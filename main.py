2e00b2380a5efcb86adeb13314c29a5c337499a9
Eduardo Imery <ejwimery@gmail.com>
2019-09-07 11:03:20 -0600

    fixing problem with a variable

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from flask import Flask, request
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    user_ip= reques.remote_addr
+    user_ip= request.remote_addr
     return 'heello from hell, tu IP es {}'.format(user_ip)
 
 @app.route('/hi')
