348668f50a201f38e11882fd8d5e8f3a6ce93315
Eduardo Imery <ejwimery@gmail.com>
2019-09-07 11:01:41 -0600

    setting a main file with basic config to run a server in flask

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from flask import Flask, request
 
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    user_ip= reques.remote_addr
+    return 'heello from hell, tu IP es {}'.format(user_ip)
+
+@app.route('/hi')
+def hi():
+    return 'Weel this is another gretting'
