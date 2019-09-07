57bbe2ea49c0b3b43a615415c8f13bf72bb785f2
Eduardo Imery <ejwimery@gmail.com>
2019-09-07 11:47:45 -0600

    creating a list to iterate in the template

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, redirect , make_response, render_template
 
 app = Flask(__name__)
 
+todos = ['TODO 1','TODO 2','TODO 3','TODO 4','TODO 5']
+
 @app.route('/')
 def index():
     user_ip= request.remote_addr 
@@ -13,4 +15,9 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return render_template('hello.html', user_ip=user_ip)
+    context = {
+        'user_ip' : user_ip,
+        'todos' : todos,
+    }
+    
+    return render_template('hello.html', **context)
