3c15ceaf1eb9c7acea68b546f85a29fe7c318eff
Eduardo Imery <ejwimery@gmail.com>
2019-09-09 19:43:55 -0600

    fixing problem with test

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_bootstrap import bootstrap
+from flask_bootstrap import Bootstrap
 from .config import Config
 
 def create_app():
     app = Flask(__name__)
-    bootstrap = Bootsrapt(app)
+    bootstrap = Bootstrap(app)
     app.config.from_object(Config)
 
     return app
