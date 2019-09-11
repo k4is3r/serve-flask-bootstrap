eae29a57350b0d34c4d1ff5ff7ebed8e5e0cfff0
Eduardo Imery <ejwimery@gmail.com>
2019-09-10 19:59:48 -0600

    creating a badge for the task and the option to delete

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_bootstrap import Bootstrap
 import unittest
 from app import create_app
 from app.forms import TodoForm
-from app.firestore_service import get_users, get_todos, put_todos
+from app.firestore_service import get_users, get_todos, put_todos, delete_todo
 from flask_login import login_required, current_user
 
 app = create_app()
@@ -48,6 +48,12 @@ def hello():
 
 
     return render_template('hello.html', **context)
+
+@app.route('/todos/delete/<todo_id>', methods=['POST'])
+def delete(todo_id):
+    user_id = current_user.id
+    delete_todo(user_id=user_id,todo_id=todo_id)
+    return redirect(url_for('hello'))
 
 @app.errorhandler(404)
 def not_found(error):
